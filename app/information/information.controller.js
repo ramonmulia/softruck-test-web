3ce5868b25a9ec6ca4cc6a677592da03b4372df8
ramon <ramonmulia@gmail.com>
2016-04-03 23:39:20 -0300

    corrections

--- app/information/information.controller.js
+++ app/information/information.controller.js
@@ -8,6 +8,7 @@
   function informationController($http, $timeout, $mdDialog, informationService) {
     var vm = this;
     vm.data = [];
+    vm.state = {};
     vm.getFuelTypeName = getFuelTypeName;
     vm.getInformation = getInformation;
     vm.showDetail = showDetail;
@@ -26,20 +27,23 @@
       informationService.getStates()
         .then(function(data) {
           vm.states = data;
+        })
+        .catch(function() {
+          informationService.showAlert('Error at getting information, please try again later.');
         });
     }
 
-    function getInformation() {
+    function getInformation(state) {
 
       vm.data = [];
       vm.loadingData = true;
 
-      informationService.getInformation()
+      informationService.getInformation(state)
         .then(function(data) {
           vm.data = data;
         })
         .catch(function() {
-
+          informationService.showAlert('Error at getting information, please try again later.');
         })
         .finally(function() {
           vm.loadingData = false;
--- app/information/information.service.js
+++ app/information/information.service.js
@@ -10,12 +10,25 @@
     return {
       getInformation: getInformation,
       getStates: getStates,
-      showInformationDetail: showInformationDetail
+      showInformationDetail: showInformationDetail,
+      showAlert: showAlert
     };
 
-    function getInformation() {
+    function getInformation(state) {
       var url = 'https://bitbucket.org/softruck/node-test/raw/91c2d054c5682887cd1c446ebad9fbc89468fe5a/output.json';
       return gatewayService.get(url);
+    }
+
+    function showAlert(message) {
+      $mdDialog.show(
+        $mdDialog.alert()
+        .parent(angular.element(document.body))
+        .clickOutsideToClose(true)
+        .title('Atention')
+        .textContent(message)
+        .ok('OK')
+        .targetEvent(ev)
+      );
     }
 
     function showInformationDetail(state, item) {
